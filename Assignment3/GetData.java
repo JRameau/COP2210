6f44f3e5ae75bbda09185e5531699096dd2ea232
JRameau <jrame002@fiu.edu>
2016-06-30 01:55:18 -0400

    Update GetData.java

--- Assignment3/GetData.java
+++ Assignment3/GetData.java
@@ -1,1 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jessica
+ */
+import javax.swing.JOptionPane;
+
+public class GetData 
+{
+    public static String getWord(String n)
+    {
+        return JOptionPane.showInputDialog(n);
+    }
+    
+    public static double getDouble(String n)
+    {
+        return Double.parseDouble(getWord(n));
+    }
+    
+    public static int getInt(String n)
+    {
+        return Integer.parseInt(getWord(n));
+    }
+}
 
